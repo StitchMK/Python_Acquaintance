c6cb183ce94cf20bbb54562f6cb10015ee687e30
Maria Schneider <maria.schneider.aug@gmail.com>
2022-12-15 21:03:22 +0100

    HomeWork - Task 1 correcteed

--- Семинары/HomeWorks/HW1/task_1.py
+++ Семинары/HomeWorks/HW1/task_1.py
@@ -1,7 +1,7 @@
 # 1. Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
 # и проверяет, является ли этот день выходным.
 
-n = int(input())
+n = int(input('Enter the number of the day of the week: '))
 if n == 6 or n == 7:
     print('Yes')
 else:
