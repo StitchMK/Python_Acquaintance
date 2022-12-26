34b59f4495e054ea4d7f89df96e775899be9cf5e
Maria Schneider <maria.schneider.aug@gmail.com>
2022-12-26 17:43:23 +0100

    Home Work 3 - Task 2 corrected

--- Семинары/HomeWorks/HW3/task3_2.py
+++ Семинары/HomeWorks/HW3/task3_2.py
@@ -2,27 +2,20 @@
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
 from random import sample
+import math
 
 def product_of_num(len_list):
     new_list = sample(range(1, len_list * 2), k=len_list)
     print(new_list)
     product_list = []
-    if len_list % 2 == 0:
-        n = len_list / 2
-    else:
-        n = int(len_list / 2) + 1
-    print(n)
+    n = int(len_list / 2)
+
 
     for i in range(0, n):
-        product_num = new_list[i] * new_list[len_list - 1 - i]
-        i += 1
-    print(new_list[i])
-    print(new_list[len_list - 1 - i])
-    print(product_num)
-
-#        product_list[n] = product_num
-#        n += 1
-
-#    return product_list
+        product_list.append(new_list[i] * new_list[len_list - 1 - i])
+    if len_list % 2 != 0:
+        product_list.append(new_list[(math.ceil(len_list / 2 - 1))])
+        
+    print(product_list)
 
 print(product_of_num(int (input('Input the length of your list: '))))
