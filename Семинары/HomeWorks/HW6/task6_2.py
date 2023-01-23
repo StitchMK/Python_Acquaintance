a01381214eeab6bdb09a0c5e357ef5f1b2eb1c59
Maria Schneider <maria.schneider.aug@gmail.com>
2023-01-23 22:48:54 +0100

    Home Work 6 task 3 added

--- Семинары/HomeWorks/HW6/task6_2.py
+++ Семинары/HomeWorks/HW6/task6_2.py
@@ -3,4 +3,4 @@
 def uniq_list(num):
     return [el for el in range(20, num + 1) if el % 20 == 0 or el % 21 == 0]
 
-print(uniq_list(int(input('Input your number: '))))
+print(uniq_list(int(input('Enter your number: '))))
--- Семинары/HomeWorks/HW6/task6_3.py
+++ Семинары/HomeWorks/HW6/task6_3.py
@@ -1,2 +1,14 @@
 # 3. Написать функцию, аргументы имена сотрудников, возвращает словарь, ключи — первые буквы имён, значения — списки, содержащие имена, начинающиеся с соответствующей буквы.
 
+def thesaurus(*args):
+    names_dict = {}
+    for i in sorted(args):
+        letter = i[0]
+        if letter not in names_dict:
+            names_dict[letter] = [i]
+        else:
+            names_dict[letter] += [i]
+    
+    return names_dict
+
+print(thesaurus("Иван", "Мария", "Петр", "Илья", "Марина", "Алина", "Бибочка"))
